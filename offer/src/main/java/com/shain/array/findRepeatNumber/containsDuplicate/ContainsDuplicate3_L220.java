520fa8227704e2762a8a9690098d59fea1d09557
Shain001 <84569049+Shain001@users.noreply.github.com>
2023-11-25 15:17:02 +1100

    update todos

--- offer/src/main/java/com/shain/array/findRepeatNumber/containsDuplicate/ContainsDuplicate3_L220.java
+++ offer/src/main/java/com/shain/array/findRepeatNumber/containsDuplicate/ContainsDuplicate3_L220.java
@@ -1,5 +1,41 @@
 package com.shain.array.findRepeatNumber.containsDuplicate;
 
-// todo: 看 treeset & bucket
+import java.util.TreeSet;
+
+// todo: 看 treeSet & bucket -> 手写treeSet; 桶排序
 public class ContainsDuplicate3_L220 {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
+        TreeSet<Integer> window = new TreeSet<>();
+
+        int left = 0;
+        int right = 0;
+
+        while (right < nums.length) {
+            while (right - left <= indexDiff && right < nums.length) {
+                int cur = nums[right];
+
+                if (window.ceiling(cur) != null && Math.abs(window.ceiling(cur) - cur) <= valueDiff) {
+                    return true;
+                }
+
+                if (window.floor(cur) != null && Math.abs(window.floor(cur) - cur) <= valueDiff) {
+                    return true;
+                }
+
+                window.add(cur);
+                right++;
+            }
+
+            window.remove(nums[left]);
+            left++;
+        }
+
+        return false;
+    }
+
+    // 利用桶排序思想
+    public boolean bucket() {
+        // todo: implement
+        return false;
+    }
 }
--- offer/src/main/java/com/shain/array/findRepeatNumber/singleNumberSeries/SingleNumber1_L136.java
+++ offer/src/main/java/com/shain/array/findRepeatNumber/singleNumberSeries/SingleNumber1_L136.java
@@ -1,5 +1,5 @@
 package com.shain.array.findRepeatNumber.singleNumberSeries;
 
-// todo:
+// todo: 位运算
 public class SingleNumber1_L136 {
 }

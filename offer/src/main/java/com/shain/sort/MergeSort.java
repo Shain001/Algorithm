f7d11b88651d1a0c1211ec2785498e89748c57db
Shain001 <84569049+Shain001@users.noreply.github.com>
2024-01-23 23:24:48 +1100

    add merge sort

---
+++ offer/src/main/java/com/shain/sort/MergeSort.java
@@ -0,0 +1,49 @@
+package com.shain.sort;
+
+import java.util.Arrays;
+
+public class MergeSort {
+    private int[] nums;
+
+    public static void main(String[] args) {
+        MergeSort test = new MergeSort();
+        System.out.println(Arrays.toString(test.mergeSort(new int[]{2})));
+    }
+
+    public int[] mergeSort(int[] nums) {
+        this.nums = nums;
+        return doMergeSort(0, nums.length - 1);
+    }
+
+    private int[] doMergeSort(int left, int right) {
+        if (left == right) {
+            return new int[]{nums[left]};
+        }
+
+        int mid = left + (right - left) / 2;
+        return merge(doMergeSort(left, mid), doMergeSort(mid + 1, right));
+    }
+
+    private int[] merge(int[] left, int[] right) {
+        int[] merged = new int[left.length + right.length];
+
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                merged[k++] = left[i++];
+            } else {
+                merged[k++] = right[j++];
+            }
+        }
+
+        while (i < left.length) {
+            merged[k++] = left[i++];
+        }
+
+        while (j < right.length) {
+            merged[k++] = right[j++];
+        }
+
+        return merged;
+    }
+}

c8058fe22943c90900d02287a0bc1ca323a23bc7
Shain001 <84569049+Shain001@users.noreply.github.com>
2024-02-18 01:26:11 +1100

    update

--- offer/src/main/java/com/shain/array/randomAlgorithms/ReservoirAlgorithm_L382.java
+++ offer/src/main/java/com/shain/array/randomAlgorithms/ReservoirAlgorithm_L382.java
@@ -15,8 +15,46 @@ import java.util.Random;
  *
  * 对于每个元素， 其是最终被选定的结果的概率= 遍历到该数字时选到他的概率 * 遍历到后面数字以后， 一直没有替换掉他的概率。
  *
- * https://labuladong.github.io/algo/di-san-zha-24031/shu-xue-yu-659f1/tan-tan-yo-b4bb5/
+ * https://labuladong.github.io/algo/frequency-interview/random-algorithm/#%E6%B0%B4%E5%A1%98%E6%8A%BD%E6%A0%B7%E7%AE%97%E6%B3%95
  * 公式推论看这个图把， 懒得写一看就懂了。
+ *
+ *
+ * 补充：
+ * 当选取的元素不是1个， 而是m个时 （也包含 数组不是一个stream， 而是从n个元素中选取m个，一样的）：
+ *
+ * 选取第i个元素的概率变为 m/i, 而不是 1/i.
+ * 证明时， 每个元素被选取的概率都是 m/n, 而不是 1/n
+ * todo： 推导证明过程。
+ *
+ * import (
+ *     "fmt"
+ *     "math/rand"
+ *     "time"
+ * )
+ *
+ * func generateRandomSample(n int, m int) []int {
+ *     rand.Seed(time.Now().UnixNano()) // 初始化随机数种子
+ *
+ *     // 初始化样本集合
+ *     sample := make([]int, m)
+ *     for i := 0; i < m; i++ {
+ *         sample[i] = i + 1
+ *     }
+ *
+ *     // 从第 m+1 个元素开始处理
+ *     for i := m + 1; i <= n; i++ {
+ *         // 以 m/i 的概率决定是否替换样本集合中的元素
+ *         r := rand.Intn(i) + 1 // 生成一个 [1, i] 范围内的随机数
+ *         if r <= m {
+ *             // 随机选择样本集合中的一个元素进行替换
+ *             replaceIndex := rand.Intn(m)
+ *             sample[replaceIndex] = i
+ *         }
+ *     }
+ *
+ *     return sample
+ * }
+ *
  */
 public class ReservoirAlgorithm_L382 {
     private int result;

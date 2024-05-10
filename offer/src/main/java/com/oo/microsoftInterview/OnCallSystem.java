c8dc492148c83ef717e9040e23a28182853a356c
Shain001 <84569049+Shain001@users.noreply.github.com>
2024-05-10 23:57:30 +1000

    oncall系统注释

--- offer/src/main/java/com/oo/microsoftInterview/OnCallSystem.java
+++ offer/src/main/java/com/oo/microsoftInterview/OnCallSystem.java
@@ -4,6 +4,35 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * 微软二轮面试， SDE II   2024 May
+ *
+ * 设计一个 oncall 系统， 系统中有三种不同的 employers,  worker, lead, and boss.
+ *
+ * incident 最开始会分配给 一个 worker， worker 可以选择自己处理， 或者escalate 给一个 lead.
+ * lead 也可以选择自己处理或者 escalate 给 boss。
+ * boss必须处理收到的incident
+ *
+ * 如果 没有 available 的worker则发送给lead， 如果lead也都不available， 直接发送给boss。
+ *
+ *
+ * 重点：
+ * 1。 策略模式的应用。
+ *
+ * 策略模式 详解： https://refactoringguru.cn/design-patterns/strategy
+ *
+ * 解释很抽象， 但是basically speaking, 当 三种不同的 employer 处理请求的时候， 逻辑基本相同， 只有 boss稍微有点区别， 即必须处理。
+ * 此处使用 策略模式， 定义一个strategy 接口， 然后 分别创建 三个class implements 这个接口， 每个class则是对应的员工种类的 应对逻辑。
+ *
+ * 然后在 handlerEmployee class 中设置 strategy field， 直接调用该接口处理 incident。
+ *
+ * 2。 incidengHandler 接口 和  Employee 抽象类的设置。
+ *
+ * 三种员工都属于 Employee, 有共同的属性， 所以抽象一个parent class。
+ * 但是 从系统扩展的角度考虑， 并非所有的Employee都可能是可以处理incdient的类型， 所以再创建一个 incidentHandler interface， 让本题中的
+ * 三种员工calss 都implement这个接口
+ *
+ */
 public class OnCallSystem {
     private List<Boss> bosses;
     private List<Worker> workers;
@@ -17,7 +46,7 @@ public class OnCallSystem {
         this.incidents = new ArrayList<>();
     }
 
-    public boolean handleIncident() throws Exception {
+    public boolean handleIncident() {
         var incident = new Incident();
 
         for (Worker w : workers) {
@@ -40,12 +69,12 @@ public class OnCallSystem {
         throw new NoAvailableHandlerException();
     }
 
-    private boolean assignIncident(HandlerEmployee e, Incident incident) {
+    private boolean assignIncident(IncidentHandler e, Incident incident) {
         e.handleIncident(incident);
         return incident.status == Incident.Status.PROCESSED;
     }
 
-    class NoAvailableHandlerException extends RuntimeException {
+    static class NoAvailableHandlerException extends RuntimeException {
         public NoAvailableHandlerException() {
             super("No Available Handler");
         }
